4cffd589509b46bcc0da6e3c422c06b4f2d091af
Alexander <aliaksandr.samsonik@gmail.com>
2018-10-22 22:11:51 +0300

    Add new files

---
+++ src/main/java/com/springcore/start/SpringCoreStartApplication.java
@@ -0,0 +1,12 @@
+package com.springcore.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreStartApplication.class, args);
+    }
+}
---
+++ src/test/java/com/springcore/start/SpringCoreStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcore.start;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCoreStartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
